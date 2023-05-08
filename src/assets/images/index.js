56435ff8d40f2b93717408ef5bd3eb9711172aed
git config --global user.name Your <muhammad.umer@surveyauto.com>
2023-05-08 22:12:38 +0500

    deploying

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,3 @@
-import blue8 from "./dot-blue-8.png";
-import green8 from "./dot-green-8.png";
-
 import conductedAt from "./conducted-at.png";
 
-export { blue8, green8, conductedAt };
+export { conductedAt };
